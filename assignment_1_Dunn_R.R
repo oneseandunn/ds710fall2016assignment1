f7ff5206d5d628fe42e37577d1eb08053cc41bbe
oneseandunn <oneseandunn@gmail.com>
2016-09-12 20:59:38 -0500

    completed homework

---
+++ assignment_1_Dunn_R.R
@@ -0,0 +1,15 @@
+2015^(1/3)
+abs((.7-6.8)/.58)
+a <-  1:12
+a #using RStudio the <- symbol make fo cleaner code
+b <- c(1,3,5,7,9,11)
+b
+c <- seq(1,11,2)
+c
+ln.a <- log(a)
+ln.a
+d <- sqrt(b)
+d
+?sd
+Name <- "Susan"
+paste("My name is", Name)
